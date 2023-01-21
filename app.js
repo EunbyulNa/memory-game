8a737a9cbde87aa9ae6aefacc23ae9e873300977
Eunbyul <las4134@gmail.com>
2023-01-21 06:37:57 +0000

    add checkcard function

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const section = document.querySelector("section");
 const playerLivesCount = document.querySelector("span");
 let playerLives = 6;
 
+
 //Add player lives count
 playerLivesCount.textContent = playerLives;
 
-//card game data, return object in an array
+//card game data
 let cardsData = [
   {imgSrc: "./imgs/01.jpg", name:"01"},
   {imgSrc: "./imgs/02.jpg", name:"02"},
@@ -21,12 +22,13 @@ let cardsData = [
   {imgSrc: "./imgs/06.jpg", name:"06"},
 ]
 
+//randomize cards data
 function random() {
   cardsData.sort( () => Math.random() - 0.5);
   return cardsData;
 }
 
-
+//Generate radom card data and display it
  function displayCards() {
     let randomCards = random();
 
@@ -43,20 +45,51 @@ function random() {
 
     //Attach the img src to faceImg div
     face.src = item.imgSrc;
+    //Attach the data name attribute to each card
+    card.setAttribute("name", item.name)
 
-    //Append child each element to parentElement
+    //Append child element to parentElement
     section.appendChild(card);
     card.appendChild(face);
     card.appendChild(back);
 
-    //each card click event 
-    card.addEventListener("click", function() {
-      card.classList.toggle('toggleCard')
-    })
-  })
+    //each card click event
+    card.addEventListener("click", function(e) {
+      card.classList.toggle('toggleCard');
+      checkCard(e)
 
+    });
+  });
+};
+
+
+function checkCard(e) {
+   let checked = e.target; //clicked card
+
+   checked.classList.add('flipped');
+   let flippedCards = document.querySelectorAll(".flipped");
+
+  if(flippedCards.length === 2){
+    if(flippedCards[0].getAttribute("name") === flippedCards[1].getAttribute("name")){
+      console.log("match");
+      flippedCards.forEach( (card) => {
+        card.classList.remove('flipped');
+        card.style.pointerEvents = "none";
+      })
+
+    }else{
+      console.log("wrong");
+      flippedCards.forEach( (card) => {
+        card.classList.remove('flipped');
+        setTimeout( () => {
+          card.classList.remove('toggleCard');
+        },1000);
+      });
+    }
+  }
 
 
 }
+
 
 displayCards()
